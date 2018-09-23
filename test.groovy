960bbb5fd89af218d642c0264398205b3c77d554
Shawn Junell <sjunell@starbucks.com>
2018-09-23 10:27:25 -0700

    update

--- test.groovy
+++ test.groovy
@@ -11,19 +11,9 @@ import groovy.json.JsonSlurper
 import javaposse.jobdsl.dsl.DslScriptLoader
 import javaposse.jobdsl.plugin.JenkinsJobManagement
 import com.microsoftopentechnologies.windowsazurestorage.*
-import hudson.maven.MavenModule
-import hubson.matrix.MatrixConfiguration
 import javaposse.jobdsl.dsl.DslScriptLoader
 import javaposse.jobdsl.plugin.JenkinsJobManagement
 
 node {
    echo 'Hello World'
-}
-
-jenkins.model.Jenkins.getInstance().getAllItems().each {
-  // MavenModule is superfluous project returned by getAllItems()
-  if (!(it instanceof hudson.maven.MavenModule || 
-        it instanceof hudson.matrix.MatrixConfiguration)) {
-      println it
-  }
 }
