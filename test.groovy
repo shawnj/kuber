0e11fd7eb9ad02ee1c7c5ca60e2ddc448cfb0b50
Shawn Junell <sjunell@starbucks.com>
2018-09-23 16:58:09 -0700

    cec

--- test.groovy
+++ test.groovy
@@ -5,14 +5,12 @@ import groovy.json.JsonSlurper
 import hudson.*
 import hudson.model.*
 import jenkins.model.*
-import javaposse.jobdsl.dsl.DslScriptLoader
-import javaposse.jobdsl.plugin.JenkinsJobManagement
 
 
 //def gitUrl = 'git://github.com/shawnj/kuber.git'
 println("Test Job")
 
-freeStyleJob('testjob') {
+job('testjob') {
     steps {
         shell('echo "hello world"')
     }
