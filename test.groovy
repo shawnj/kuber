aeaec89540ac86d9cebe9376e8d5357d551116f9
Shawn Junell <sjunell@starbucks.com>
2018-09-23 17:15:16 -0700

    upd

--- test.groovy
+++ test.groovy
@@ -1,8 +1,10 @@
 //def gitUrl = 'git://github.com/shawnj/kuber.git'
 println("Test Job")
 
-job('testjob') {
-    steps {
-        shell('echo "hello world"')
+node{
+    job('testjob') {
+        steps {
+            shell('echo "hello world"')
+        }
     }
 }
