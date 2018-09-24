7cb495add5d70c6aa3a8252f5343a54b6075d993
Shawn Junell <sjunell@starbucks.com>
2018-09-23 19:58:51 -0700

    eee

--- test.groovy
+++ test.groovy
@@ -1,11 +1,20 @@
+
+import com.cloudbees.hudson.plugins.folder.*
+import groovy.json.JsonSlurper
+import hudson.*
+import hudson.model.*
+import jenkins.model.*
+
 //def gitUrl = 'git://github.com/shawnj/kuber.git'
 println("Test Job")
+
+def hello = 'echo hello there'
 
 node{
     //job('testjob'){
     stage("one"){
         //steps{
-            sh('echo "hello world"')
+            sh(hello)
         //}
     }
     //}
