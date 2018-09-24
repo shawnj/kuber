40c44472f0b02f7870c210a249317440bf21b017
Shawn Junell <sjunell@starbucks.com>
2018-09-23 17:11:24 -0700

    sd

--- test.groovy
+++ test.groovy
@@ -1,12 +1,3 @@
-#!/usr/bin/env groovy
-
-import com.cloudbees.hudson.plugins.folder.*
-import groovy.json.JsonSlurper
-import hudson.*
-import hudson.model.*
-import jenkins.model.*
-
-
 //def gitUrl = 'git://github.com/shawnj/kuber.git'
 println("Test Job")
 
